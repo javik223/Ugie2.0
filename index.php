3436160613fec13809abeba3f7744e1cdf50ffb6
Victory James <javik@live.com>
2014-05-20 12:48:46 +0100

    Expressionengine Installed

--- index.php
+++ index.php
@@ -1,264 +1,191 @@
-<? include_once("header.php"); ?>
-    
-    <!-- Slider for homepage. This slider contains an image and caption text that changes after a chosen period of time-->
-    <div class="slider row collapse js-slider">
-        <div class="slider-wrapper js-slider-wrapper cycle-slideshow" data-cycle-prev=".js-slider-prev" data-cycle-next=".js-slider-next" data-cycle-slides=".js-slider-item">
-            <div class="slider-item js-slider-item">
-            <div class="slider-item-image">
-                <img src="/assets/img/ugie-carnival-street-performance.jpg" alt="Traditional Groups Dancing at Ugie Carnival">
-            </div>
-
-            <h2 class="slider-item-caption">
-                A Celebration of Fun &amp; Excitement
-            </h2>
-        </div>
-
-        <div class="slider-item js-slider-item">
-            <div class="slider-item-image">
-                <img src="/assets/img/ugie-carnival-street-performance2.jpg" alt="Traditional Groups Dancing at Ugie Carnival">
-            </div>
-
-            <h2 class="slider-item-caption">
-                A Celebration of Fun &amp; Excitement
-            </h2>
-        </div>
-        </div>
-
-        <div class="slider-controls">
-            <span class="icon-arrow-left js-slider-prev"></span>
-
-            <span class="icon-arrow-right js-slider-next"></span>
-        </div>
-    </div>
-    <!--End of Slider -->
-
-
-    <!--Media Navigation begins-->
-        <div class="media-nav row collapse">
-        <ul class="media-nav_links">
-            <li><a href="/media" class="isActive">Media</a></li>
-            <li><a href="/media/photos">Photos</a></li>
-            <li><a href="/media/videos">Videos</a></li>
-            <li><a href="/events">Lineup</a></li>
-        </ul>
-
-        <div class="media-nav_view-media">
-             <button class="small button button-secondary round">
-               View all media <span class="arrow-right"></span>
-            </button>
-        </div>
-    </div>
-    <!--Media Navigation ends-->
-
-    <!--Featured Media Items Begins-->
-
-    <div class="row featured collapse">
-        <div class="featured-item medium-4 column js-fmi">
-            <img src="/assets/img/performances-at-ugie.jpg" alt="Performances at the Ugie festival">
-
-            <div class="featured-item_caption">
-                <h5>Photos</h5>
-
-                <p>
-                    Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
-                </p>
-            </div>
-        </div>
-
-        <div class="featured-item medium-4 column js-fmi">
-            <img src="/assets/img/performances-at-ugie2.jpg" alt="Performances at the Ugie festival">
-
-            <div class="featured-item_caption">
-                <h5>Photos</h5>
-
-                <p>
-                    Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
-                </p>
-            </div>
-        </div>
-
-        <div class="featured-item medium-4 column js-fmi">
-            <img src="/assets/img/performances-at-ugie3.jpg" alt="Performances at the Ugie festival">
-
-            <div class="featured-item_caption">
-                <h5>Photos</h5>
-
-                <p>
-                    Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
-                </p>
-            </div>
-        </div>
-    </div>
-
-    <div class="row collapse featured">
-        <div class="featured-item medium-5 column js-fmi">
-            <img src="/assets/img/performances-at-ugie4.jpg" alt="Performances at the Ugie festival">
-
-            <div class="featured-item_caption">
-                <h5>Photos</h5>
-
-                <p>
-                    Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
-                </p>
-            </div>
-        </div>
+<?php
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2003 - 2013, EllisLab, Inc.
+ * @license		http://ellislab.com/expressionengine/user-guide/license.html
+ * @link		http://ellislab.com
+ * @since		Version 2.0
+ */
+
+/*
+ * --------------------------------------------------------------------
+ *  System Path
+ * --------------------------------------------------------------------
+ *
+ * The following variable contains the server path to your
+ * ExpressionEngine "system" folder.  By default the folder is named
+ * "system" but it can be renamed or moved for increased security.
+ * Indicate the new name and/or path here. The path can be relative
+ * or it can be a full server path.
+ *
+ * http://ellislab.com/expressionengine/user-guide/installation/best_practices.html
+ *
+ */
+	$system_path = './ugie-admin';
+
+
+/*
+ * --------------------------------------------------------------------
+ *  Multiple Site Manager
+ * --------------------------------------------------------------------
+ *
+ * Uncomment the following variables if you are using the Multiple
+ * Site Manager: http://ellislab.com/expressionengine/user-guide/cp/sites
+ *
+ * Set the Short Name of the site this file will display, the URL of
+ * this site's admin.php file, and the main URL of the site (without
+ * index.php)
+ *
+ */
+ //  $assign_to_config['site_name']  = 'domain2_short_name';
+ //  $assign_to_config['cp_url'] = 'http://domain2.com/admin.php';
+ //  $assign_to_config['site_url'] = 'http://domain2.com';
+
+
+/*
+ * --------------------------------------------------------------------
+ *  Error Reporting
+ * --------------------------------------------------------------------
+ *
+ * PHP and database errors are normally displayed dynamically based
+ * on the authorization level of each user accessing your site.
+ * This variable allows the error reporting system to be overridden,
+ * which can be useful for low level debugging during site development,
+ * since errors happening before a user is authenticated will not normally
+ * be shown.  Options:
+ *
+ *	$debug = 0;  Default setting. Errors shown based on authorization level
+ *
+ *	$debug = 1;  All errors shown regardless of authorization
+ *
+ * NOTE: Enabling this override can have security implications.
+ * Enable it only if you have a good reason to.
+ *
+ */
+	$debug = 0;
+
+/*
+ * --------------------------------------------------------------------
+ *  CUSTOM CONFIG VALUES
+ * --------------------------------------------------------------------
+ *
+ * The $assign_to_config array below will be passed dynamically to the
+ * config class. This allows you to set custom config items or override
+ * any default config values found in the config.php file.  This can
+ * be handy as it permits you to share one application between more then
+ * one front controller file, with each file containing different
+ * config values.
+ *
+ * Un-comment the $assign_to_config array below to use this feature
+ *
+ * NOTE: This feature can be used to run multiple EE "sites" using
+ * the old style method.  Instead of individual variables you'll
+ * set array indexes corresponding to them.
+ *
+ */
+//	$assign_to_config['template_group'] = '';
+//	$assign_to_config['template'] = '';
+//	$assign_to_config['site_index'] = '';
+//	$assign_to_config['site_404'] = '';
+//	$assign_to_config['global_vars'] = array(); // This array must be associative
+
+
+/*
+ * --------------------------------------------------------------------
+ *  END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
+ * --------------------------------------------------------------------
+ */
+
 
-       <div class="medium-7 columns">
-            <div class="row collapse">
-            <div class="featured-item medium-6 column js-fmi">
-                <img src="/assets/img/performances-at-ugie5.jpg" alt="Performances at the Ugie festival">
-
-                <div class="featured-item_caption">
-                    <h5>Photos</h5>
-
-                    <p>
-                        Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
-                    </p>
-                </div>
-            </div>
-
-            <div class="featured-item medium-6 column js-fmi">
-                <img src="/assets/img/performances-at-ugie5.jpg" alt="Performances at the Ugie festival">
-
-                <div class="featured-item_caption">
-                    <h5>Photos</h5>
-
-                    <p>
-                        Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
-                    </p>
-                </div>
-            </div>
-
-            <div class="featured-item medium-12 column js-fmi">
-                <img src="/assets/img/performances-at-ugie6.jpg" alt="Performances at the Ugie festival">
-
-                <div class="featured-item_caption">
-                    <h5>Photos</h5>
-
-                    <p>
-                        Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
-                    </p>
-                </div>
-            </div>
-        </div>
-       </div>
-    </div>
-    <!--Featured Media Items Ends-->
-
-    <!--Featured Sections -->
-
-    <div class="row collapse">
-        <h2 class="emt">Press, News &amp; Blog</h2>
-
-        <div class="row main-content">
-            <div class="featured-news small-12 medium-8 large-6 columns">
-                <div class="featured-news_item">
-                    <div class="featured-news_item_image">
-                        <img src="/assets/img/news-blog-image.jpg" alt="">
-                    </div>
-
-                    <div class="featured-news_item_content">
-                        <h3 class="featured-news_item_heading">Live: DCC Fashion Show</h3>
-
-                        <p>Designers Auranela and CrownLekAfrik have just showcased their designs for charity in the 2013 Destiny's Child Centre Fashion Show, currently holding at the Tinapa Emporium in Calabar.</p>
-
-                    </div>
-                </div>
-
-                <div class="featured-news_item">
-                    <div class="featured-news_item_image">
-                        <img src="/assets/img/news-blog-image2.jpg" alt="">
-                    </div>
-
-                    <div class="featured-news_item_content">
-                        <h3 class="featured-news_item_heading">Live: DCC Fashion Show</h3>
-
-                        <p>Designers Auranela and CrownLekAfrik have just showcased their designs for charity in the 2013 Destiny's Child Centre Fashion Show, currently holding at the Tinapa Emporium in Calabar.</p>
-
-                    </div>
-                </div>
-
-                <div class="featured-news_item">
-                    <div class="featured-news_item_image">
-                        <img src="/assets/img/news-blog-image3.jpg" alt="">
-                    </div>
-
-                    <div class="featured-news_item_content">
-                        <h3 class="featured-news_item_heading">Live: DCC Fashion Show</h4>
-
-                        <p>Designers Auranela and CrownLekAfrik have just showcased their designs for charity in the 2013 Destiny's Child Centre Fashion Show, currently holding at the Tinapa Emporium in Calabar.</p>
-
-                    </div>
-                </div>
-
-                <div class="featured-news_item">
-                    <div class="featured-news_item_image">
-                        <img src="/assets/img/news-blog-image3.jpg" alt="">
-                    </div>
-
-                    <div class="featured-news_item_content">
-                        <h3 class="featured-news_item_heading">Live: DCC Fashion Show</h4>
-
-                        <p>Designers Auranela and CrownLekAfrik have just showcased their designs for charity in the 2013 Destiny's Child Centre Fashion Show, currently holding at the Tinapa Emporium in Calabar.</p>
-
-                    </div>
-                </div>
-            </div>
-
-          <!--Social media updates and newsletter signup -->
-        <div class="updates medium-4 large-3 columns">
-            <div class="update update-twitter">
-                <h4>From Twitter <a href="http://twitter.com/thebeninugiecarnival"><span class="icon-twitter"></span></a></h4>
-
-                <p>In case you're after an amusing start to your Saturday evening, here's some Milton Jones highlights <a href="#gb37http://t.co/rMTjdz1U0A">#gb37 http://t.co/rMTjdz1U0A</a></p>
-            </div>
-
-            <div class="update update-newsletter">
-                <h4>Get Updates</h4>
-
-                <p>Signup to receive the latest Ugie Carnival News, along with special updates and information.</p>
-
-                <form action="">
-                    <input type="email" name="email" placeholder="Enter your email">
-
-                    <input type="submit" value="SIGN UP" class="small button round">
-                </form>
-            </div>
-        </div>
-        <!--End of Social media udpates and newsletter signup-->
-
-        <div class="connects medium-4 large-3 columns">
-            <div class="connect connect-instagram">
-                <img src="/assets/img/instagram-logo-with-text.svg" alt="The Ugie Carnival on Instagram">
-
-                <p>
-                    Follow The Benin Ugie Carnival on Instagram, experience historical moments and share those experiences with friends
-                </p>
-            </div>
-
-            <div class="connect connect-social">
-                <h5>@TheBeninUgieCarnival</h5>
-
-                <span class="icon-twitter icon-medium"></span>
-
-                <span class="icon-instagram icon-medium"></span>
-
-                <span class="icon-facebook icon-medium"></span>
-            </div>
-        </div>
-
-        </div>
-    </div>
-
-    <!--Featured Sctions End-->
-
-
-    <!--featured ad begins here -->
-    <div class="featured-ad row" role="banner">
-        <a href="/info/history" data-title="The Benin Ugie - The Origins" title="The Benin Ugie - The Origins">
-            <img src="/assets/img/the-benin-ugie-teaser.jpg" alt="The Benin Ugie - The Origins">
-        </a>
-    </div>
-    
-
-<? include_once("footer.php"); ?>
+/*
+ * ---------------------------------------------------------------
+ *  Disable all routing, send everything to the frontend
+ * ---------------------------------------------------------------
+ */
+	$routing['directory'] = '';
+	$routing['controller'] = 'ee';
+	$routing['function'] = 'index';
+
+/*
+ * --------------------------------------------------------------------
+ *  Mandatory config overrides
+ * --------------------------------------------------------------------
+ */
+	$assign_to_config['subclass_prefix'] = 'EE_';
+
+/*
+ * --------------------------------------------------------------------
+ *  Resolve the system path for increased reliability
+ * --------------------------------------------------------------------
+ */
+
+	if (realpath($system_path) !== FALSE)
+	{
+		$system_path = realpath($system_path).'/';
+	}
+
+	// ensure there's a trailing slash
+	$system_path = rtrim($system_path, '/').'/';
+
+	// Is the sytsem path correct?
+	if ( ! is_dir($system_path))
+	{
+		exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
+	}
+
+/*
+ * --------------------------------------------------------------------
+ *  Now that we know the path, set the main constants
+ * --------------------------------------------------------------------
+ */
+	// The name of THIS file
+	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+
+	// The PHP file extension
+	define('EXT', '.php');
+
+ 	// Path to the system folder
+	define('BASEPATH', str_replace("\\", "/", $system_path.'codeigniter/system/'));
+
+	// Path to the "application" folder
+	define('APPPATH', $system_path.'expressionengine/');
+
+	// Path to the front controller (this file)
+	define('FCPATH', str_replace(SELF, '', __FILE__));
+
+	// Name of the "system folder"
+	define('SYSDIR', trim(strrchr(trim(str_replace("\\", "/", $system_path), '/'), '/'), '/'));
+
+	// The $debug value as a constant for global access
+	define('DEBUG', $debug);  unset($debug);
+
+/*
+ * --------------------------------------------------------------------
+ *  Set the error reporting level
+ * --------------------------------------------------------------------
+ */
+	if (DEBUG == 1)
+	{
+		error_reporting(E_ALL);
+		@ini_set('display_errors', 1);
+	}
+	else
+	{
+		error_reporting(0);
+	}
+
+/*
+ *---------------------------------------------------------------
+ * LOAD THE BOOTSTRAP FILE
+ *---------------------------------------------------------------
+ *
+ * And away we go...
+ *
+ */
+	require_once BASEPATH.'core/CodeIgniter'.EXT;
+
+/* End of file index.php */
+/* Location: ./index.php */
---
+++ index.php5
@@ -0,0 +1,264 @@
+<? include_once("header.php"); ?>
+    
+    <!-- Slider for homepage. This slider contains an image and caption text that changes after a chosen period of time-->
+    <div class="slider row collapse js-slider">
+        <div class="slider-wrapper js-slider-wrapper cycle-slideshow" data-cycle-prev=".js-slider-prev" data-cycle-next=".js-slider-next" data-cycle-slides=".js-slider-item">
+            <div class="slider-item js-slider-item">
+            <div class="slider-item-image">
+                <img src="/assets/img/ugie-carnival-street-performance.jpg" alt="Traditional Groups Dancing at Ugie Carnival">
+            </div>
+
+            <h2 class="slider-item-caption">
+                A Celebration of Fun &amp; Excitement
+            </h2>
+        </div>
+
+        <div class="slider-item js-slider-item">
+            <div class="slider-item-image">
+                <img src="/assets/img/ugie-carnival-street-performance2.jpg" alt="Traditional Groups Dancing at Ugie Carnival">
+            </div>
+
+            <h2 class="slider-item-caption">
+                A Celebration of Fun &amp; Excitement
+            </h2>
+        </div>
+        </div>
+
+        <div class="slider-controls">
+            <span class="icon-arrow-left js-slider-prev"></span>
+
+            <span class="icon-arrow-right js-slider-next"></span>
+        </div>
+    </div>
+    <!--End of Slider -->
+
+
+    <!--Media Navigation begins-->
+        <div class="media-nav row collapse">
+        <ul class="media-nav_links">
+            <li><a href="/media" class="isActive">Media</a></li>
+            <li><a href="/media/photos">Photos</a></li>
+            <li><a href="/media/videos">Videos</a></li>
+            <li><a href="/events">Lineup</a></li>
+        </ul>
+
+        <div class="media-nav_view-media">
+             <button class="small button button-secondary round">
+               View all media <span class="arrow-right"></span>
+            </button>
+        </div>
+    </div>
+    <!--Media Navigation ends-->
+
+    <!--Featured Media Items Begins-->
+
+    <div class="row featured collapse">
+        <div class="featured-item medium-4 column js-fmi">
+            <img src="/assets/img/performances-at-ugie.jpg" alt="Performances at the Ugie festival">
+
+            <div class="featured-item_caption">
+                <h5>Photos</h5>
+
+                <p>
+                    Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
+                </p>
+            </div>
+        </div>
+
+        <div class="featured-item medium-4 column js-fmi">
+            <img src="/assets/img/performances-at-ugie2.jpg" alt="Performances at the Ugie festival">
+
+            <div class="featured-item_caption">
+                <h5>Photos</h5>
+
+                <p>
+                    Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
+                </p>
+            </div>
+        </div>
+
+        <div class="featured-item medium-4 column js-fmi">
+            <img src="/assets/img/performances-at-ugie3.jpg" alt="Performances at the Ugie festival">
+
+            <div class="featured-item_caption">
+                <h5>Photos</h5>
+
+                <p>
+                    Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row collapse featured">
+        <div class="featured-item medium-5 column js-fmi">
+            <img src="/assets/img/performances-at-ugie4.jpg" alt="Performances at the Ugie festival">
+
+            <div class="featured-item_caption">
+                <h5>Photos</h5>
+
+                <p>
+                    Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
+                </p>
+            </div>
+        </div>
+
+       <div class="medium-7 columns">
+            <div class="row collapse">
+            <div class="featured-item medium-6 column js-fmi">
+                <img src="/assets/img/performances-at-ugie5.jpg" alt="Performances at the Ugie festival">
+
+                <div class="featured-item_caption">
+                    <h5>Photos</h5>
+
+                    <p>
+                        Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
+                    </p>
+                </div>
+            </div>
+
+            <div class="featured-item medium-6 column js-fmi">
+                <img src="/assets/img/performances-at-ugie5.jpg" alt="Performances at the Ugie festival">
+
+                <div class="featured-item_caption">
+                    <h5>Photos</h5>
+
+                    <p>
+                        Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
+                    </p>
+                </div>
+            </div>
+
+            <div class="featured-item medium-12 column js-fmi">
+                <img src="/assets/img/performances-at-ugie6.jpg" alt="Performances at the Ugie festival">
+
+                <div class="featured-item_caption">
+                    <h5>Photos</h5>
+
+                    <p>
+                        Ugie Ewere (Bringing the ewere leaves to the palace). Ugie Carnival
+                    </p>
+                </div>
+            </div>
+        </div>
+       </div>
+    </div>
+    <!--Featured Media Items Ends-->
+
+    <!--Featured Sections -->
+
+    <div class="row collapse">
+        <h2 class="emt">Press, News &amp; Blog</h2>
+
+        <div class="row main-content">
+            <div class="featured-news small-12 medium-8 large-6 columns">
+                <div class="featured-news_item">
+                    <div class="featured-news_item_image">
+                        <img src="/assets/img/news-blog-image.jpg" alt="">
+                    </div>
+
+                    <div class="featured-news_item_content">
+                        <h3 class="featured-news_item_heading">Live: DCC Fashion Show</h3>
+
+                        <p>Designers Auranela and CrownLekAfrik have just showcased their designs for charity in the 2013 Destiny's Child Centre Fashion Show, currently holding at the Tinapa Emporium in Calabar.</p>
+
+                    </div>
+                </div>
+
+                <div class="featured-news_item">
+                    <div class="featured-news_item_image">
+                        <img src="/assets/img/news-blog-image2.jpg" alt="">
+                    </div>
+
+                    <div class="featured-news_item_content">
+                        <h3 class="featured-news_item_heading">Live: DCC Fashion Show</h3>
+
+                        <p>Designers Auranela and CrownLekAfrik have just showcased their designs for charity in the 2013 Destiny's Child Centre Fashion Show, currently holding at the Tinapa Emporium in Calabar.</p>
+
+                    </div>
+                </div>
+
+                <div class="featured-news_item">
+                    <div class="featured-news_item_image">
+                        <img src="/assets/img/news-blog-image3.jpg" alt="">
+                    </div>
+
+                    <div class="featured-news_item_content">
+                        <h3 class="featured-news_item_heading">Live: DCC Fashion Show</h4>
+
+                        <p>Designers Auranela and CrownLekAfrik have just showcased their designs for charity in the 2013 Destiny's Child Centre Fashion Show, currently holding at the Tinapa Emporium in Calabar.</p>
+
+                    </div>
+                </div>
+
+                <div class="featured-news_item">
+                    <div class="featured-news_item_image">
+                        <img src="/assets/img/news-blog-image3.jpg" alt="">
+                    </div>
+
+                    <div class="featured-news_item_content">
+                        <h3 class="featured-news_item_heading">Live: DCC Fashion Show</h4>
+
+                        <p>Designers Auranela and CrownLekAfrik have just showcased their designs for charity in the 2013 Destiny's Child Centre Fashion Show, currently holding at the Tinapa Emporium in Calabar.</p>
+
+                    </div>
+                </div>
+            </div>
+
+          <!--Social media updates and newsletter signup -->
+        <div class="updates medium-4 large-3 columns">
+            <div class="update update-twitter">
+                <h4>From Twitter <a href="http://twitter.com/thebeninugiecarnival"><span class="icon-twitter"></span></a></h4>
+
+                <p>In case you're after an amusing start to your Saturday evening, here's some Milton Jones highlights <a href="#gb37http://t.co/rMTjdz1U0A">#gb37 http://t.co/rMTjdz1U0A</a></p>
+            </div>
+
+            <div class="update update-newsletter">
+                <h4>Get Updates</h4>
+
+                <p>Signup to receive the latest Ugie Carnival News, along with special updates and information.</p>
+
+                <form action="">
+                    <input type="email" name="email" placeholder="Enter your email">
+
+                    <input type="submit" value="SIGN UP" class="small button round">
+                </form>
+            </div>
+        </div>
+        <!--End of Social media udpates and newsletter signup-->
+
+        <div class="connects medium-4 large-3 columns">
+            <div class="connect connect-instagram">
+                <img src="/assets/img/instagram-logo-with-text.svg" alt="The Ugie Carnival on Instagram">
+
+                <p>
+                    Follow The Benin Ugie Carnival on Instagram, experience historical moments and share those experiences with friends
+                </p>
+            </div>
+
+            <div class="connect connect-social">
+                <h5>@TheBeninUgieCarnival</h5>
+
+                <span class="icon-twitter icon-medium"></span>
+
+                <span class="icon-instagram icon-medium"></span>
+
+                <span class="icon-facebook icon-medium"></span>
+            </div>
+        </div>
+
+        </div>
+    </div>
+
+    <!--Featured Sctions End-->
+
+
+    <!--featured ad begins here -->
+    <div class="featured-ad row" role="banner">
+        <a href="/info/history" data-title="The Benin Ugie - The Origins" title="The Benin Ugie - The Origins">
+            <img src="/assets/img/the-benin-ugie-teaser.jpg" alt="The Benin Ugie - The Origins">
+        </a>
+    </div>
+    
+
+<? include_once("footer.php"); ?>
